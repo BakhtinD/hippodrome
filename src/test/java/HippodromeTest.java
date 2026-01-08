27ee1fb19ffeae2d52293b7d59a5dceb12920f0b
Denis <denisbakhtin1@gmail.com>
2026-01-08 12:32:58 +0300

    V1

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 class HippodromeTest {
 
@@ -13,16 +18,28 @@ class HippodromeTest {
         for (int i = 0; i < 30; i++) {
             int randomInt = ThreadLocalRandom.current().nextInt(127);
             char character = (char) randomInt;
-            Horse horse = new Horse(Character.toString(character), randomInt);
+            Horse horse = new Horse(character + "a", randomInt);
             list.add(horse);
         }
         Hippodrome hippodrome = new Hippodrome(list);
         List<Horse> got = hippodrome.getHorses();
         assertEquals(got, list);
+        assertEquals(got.get(1), list.get(1));
     }
 
     @Test
+    @ExtendWith(MockitoExtension.class)
     void move() {
+        List<Horse> list = new ArrayList<>(30);
+        for (int i = 0; i < 30; i++) {
+            int randomInt = ThreadLocalRandom.current().nextInt(127);
+            char character = (char) randomInt;
+
+            Horse horse = new Horse(character + "a", randomInt);
+            list.add(horse);
+        }
+        Hippodrome hippodrome = new Hippodrome(list);
+        List<Horse> got = hippodrome.getHorses();
     }
 
     @Test
